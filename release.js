43e14a39e5abf05485054ca591de07b1d7ac903c
Daybrush <daybrush@gmail.com>
2019-04-08 02:10:46 +0900

    fix: fix init option

--- release.js
+++ release.js
@@ -15,6 +15,6 @@ const shell = require('./utils').shell;
 const flags = args.parse(process.argv);
 
 
-const addFlag = flags.init ? "" : "-add";
+const addFlag = flags.init ? "" : "--add";
 shell(`gh-pages -d ./demo --dest=./ ${addFlag} --remote ${flags.remote}`);
 
