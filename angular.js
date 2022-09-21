b1de83d6d991dd40f2ad9e6c1a9ad1f0b171889e
Daybrush <younkue.choi@navercorp.com>
2022-09-22 00:27:52 +0900

    feat: add angular file

---
+++ angular.js
@@ -0,0 +1,55 @@
+const ts = require("typescript");
+const path = require("path");
+const { writeFileSync } = require("fs");
+
+exports.convertProperties = (
+    staticConsts,
+    paths,
+) => {
+    const program = ts.createProgram(paths, {
+        target: ts.ScriptTarget.ES5,
+        module: ts.ModuleKind.CommonJS
+      });
+
+      let checker = program.getTypeChecker();
+      let replacers = [];
+
+      function visit(node) {
+        if (node.kind === ts.SyntaxKind.FirstStatement) {
+          node.declarationList.declarations.forEach(subNode => visit(subNode));
+        }
+        if (node.kind === ts.SyntaxKind.VariableDeclaration) {
+          const symbol = checker.getSymbolAtLocation(node.name);
+          const propertyName = symbol.escapedName;
+          if (staticConsts[propertyName]) {
+
+              const value = JSON.stringify(staticConsts[propertyName]);
+            replacers.push({
+              range: [node.name.end, node.initializer.end],
+              text: `: ${value} = ${value}`,
+            });
+          }
+        }
+      }
+
+      // Visit every sourceFile in the program
+      for (const sourceFile of program.getSourceFiles()) {
+        if (sourceFile.isDeclarationFile) {
+          continue;
+        }
+        replacers = [];
+        ts.forEachChild(sourceFile, visit);
+
+        let source = sourceFile.text;
+        replacers.sort((a, b) => {
+          return b.range[0] - a.range[0];
+        });
+        replacers.forEach(({ range, text }) => {
+          source = `${source.substring(0, range[0])}${text}${source.substring(range[1])}`;
+        });
+        if (replacers.length) {
+          writeFileSync(sourceFile.fileName, source, { encoding: "utf8" });
+        }
+      }
+
+}
